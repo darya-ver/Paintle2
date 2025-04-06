01044de52470c9d529b439c91e24f996e3301717
Cyrus Cowley <cyficowley@gmail.com>
2025-04-06 16:47:20 -0700

    adding images and dynamically choosing them

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,43 @@ import "./App.css";
 import { GuessBox } from "./Components/GuessBox";
 import { Header } from "./Components/Header";
 import { PaintingOfTheDay } from "./Components/PaintingOfTheDay";
-import painting from "./assets/babs.webp";
+import painting_data from './assets/final_image_data.json'
+import { useEffect, useState } from 'react'
+
 
 function App() {
   const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const startDate = new Date(2025, 3, 6) // day that we wrote this code
+  const now = new Date()
+
+  // The days between the startDate and now
+  const days = Math.floor(
+    (now.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)
+  )
+
+  const selectedAnswer = painting_data[days % painting_data.length]
+
+  const shuffledAnswers = [...painting_data]
+  for (let i = shuffledAnswers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+      ;[shuffledAnswers[i], shuffledAnswers[j]] = [
+        shuffledAnswers[j],
+        shuffledAnswers[i],
+      ]
+  }
+
+  const [image, setImage] = useState(null)
+
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const response = await import(`./assets/images/${selectedAnswer.filename}`) // change relative path to suit your needs
+      setImage(response.default)
+    }
+
+    fetchImage()
+  }, [selectedAnswer.filename])
 
   return (
     <div
@@ -25,7 +58,9 @@ function App() {
           width: "100%",
         }}
       >
-        <PaintingOfTheDay imageUrl={painting} containerRef={containerRef} />
+        {image != "" ?
+          <PaintingOfTheDay imageUrl={image} containerRef={containerRef} /> : null
+        }
       </div>
       <GuessBox />
     </div>
