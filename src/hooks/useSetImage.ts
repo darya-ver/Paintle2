0df10c2f4e7dc3bb2eebdb989a40144ee2e97c2e
Darya Verzhbinsky <daryaver@retool.com>
2025-04-16 14:55:19 -0700

    another attempt

--- src/hooks/useSetImage.ts
+++ src/hooks/useSetImage.ts
@@ -10,11 +10,12 @@ export const useSetImage = ({ selectedAnswer }: UseSetImageProps) => {
 
   useEffect(() => {
     // What it's giving: https://darya-ver.github.io/public/images/image_0011.webp
+    //                   https://darya-ver.github.io/images/image_0011.webp
     // What it needs:    https://darya-ver.github.io/Paintle2/images/image_0000.webp
     const fetchImage = async () => {
       const response = await import(
         /* @vite-ignore */
-        `/images/${selectedAnswer.filename}`
+        `/Paintle2/images/${selectedAnswer.filename}`
       );
       setImage(response.default);
     };
